d3c2817309136e9a3fa71863bf49d71ff511e38a
Vlad Lungu <vlad.lungu@windriver.com>
2021-11-09 13:49:05 +0200

    Added some meat to the virtio-rng driver
    Signed-off-by: Vlad Lungu <vlad.lungu@windriver.com>

--- drivers/virtio/virtio-rng.c
+++ drivers/virtio/virtio-rng.c
@@ -13,11 +13,56 @@
 
 #define DT_DRV_COMPAT virtio_rng
 
+#define VQIN_SIZE 2
+
+#define DEV_CFG(dev) ((struct virtio_rng_config*)(dev->config))
+#define DEV_DATA(dev) ((struct virtio_rng_data*)(dev->data))
+
+struct virtio_rng_config {
+    const struct device *bus;
+    int vq_count;
+    struct virtqueue **vqs;
+};
+
+struct virtio_rng_data {
+    struct virtqueue *vqin;
+};
+
 static int virtio_rng_init(const struct device *dev)
 {
     uint32_t devid, features;
 
     printk("%s()\n",__FUNCTION__);
+
+    __ASSERT(DEV_CFG(dev)->bus != NULL, "DEV_CFG(dev)->bus != NULL");
+    if (!device_is_ready(DEV_CFG(dev)->bus))
+        return -1;
+    printk("bus %p\n", DEV_CFG(dev)->bus);
+    devid = virtio_get_devid(DEV_CFG(dev)->bus);
+    if (devid != VIRTIO_ID_ENTROPY)
+        {
+        printk("Bad devid %08x\n", devid);
+        return -1;
+        }
+    virtio_set_status(DEV_CFG(dev)->bus, VIRTIO_CONFIG_STATUS_DRIVER);
+    virtio_set_features(DEV_CFG(dev)->bus, 0/*VIRTIO_F_NOTIFY_ON_EMPTY*/);
+    features =virtio_get_features(DEV_CFG(dev)->bus);
+    printk("features: %08x\n", features);
+
+    DEV_DATA(dev)->vqin =
+        virtio_setup_virtqueue(
+            DEV_CFG(dev)->bus,
+            0,
+            DEV_CFG(dev)->vqs[0],
+            NULL,
+            NULL
+            );
+    if (!DEV_DATA(dev)->vqin)
+        return -1;
+    virtio_register_device(DEV_CFG(dev)->bus, DEV_CFG(dev)->vq_count, DEV_CFG(dev)->vqs);
+    virtio_set_status(DEV_CFG(dev)->bus, VIRTIO_CONFIG_STATUS_DRIVER_OK);
+
+    virtqueue_notify(DEV_DATA(dev)->vqin);
 
     return 0;
 }
@@ -32,12 +77,21 @@ static const struct entropy_driver_api virtio_rng_api = {
 };
 
 #define CREATE_VIRTIO_RNG_DEVICE(inst) \
+    VQ_DECLARE(vq0_##inst, VQIN_SIZE, 4096);\
+    static struct virtqueue *vq_list_##inst[] = {VQ_PTR(vq0_##inst)};\
+    static const struct virtio_rng_config virtio_rng_cfg_##inst = {\
+        .bus = DEVICE_DT_GET(DT_BUS(DT_INST(inst, DT_DRV_COMPAT))),\
+        .vq_count = 1,\
+        .vqs = &vq_list_##inst[0],\
+        };\
+    static struct virtio_rng_data virtio_rng_data_##inst = {\
+        };\
     DEVICE_DT_INST_DEFINE(	inst,\
     virtio_rng_init,\
     NULL,\
-    NULL/*data*/,\
-    NULL/*cfg*/,\
-    PRE_KERNEL_1,\
+    &virtio_rng_data_##inst,\
+    &virtio_rng_cfg_##inst,\
+    PRE_KERNEL_2/*PRE_KERNEL_1*/,\
     CONFIG_ENTROPY_INIT_PRIORITY,\
     &virtio_rng_api);
 
