aed035a68aa95cc3bb7143385626fbc3d3f63d49
Vlad Lungu <vlad.lungu@windriver.com>
2021-11-10 13:18:41 +0200

    virtio-rng working
    Signed-off-by: Vlad Lungu <vlad.lungu@windriver.com>

--- drivers/virtio/virtio-rng.c
+++ drivers/virtio/virtio-rng.c
@@ -67,13 +67,42 @@ static int virtio_rng_init(const struct device *dev)
     return 0;
 }
 
-static int virtio_rng_get_entropy(const struct device *dev, uint8_t *buffer, uint16_t length)
+static int virtio_rng_get_entropy_internal(const struct device *dev, uint8_t *buffer, uint16_t length)
 {
+    void *cookie;
+    /*Not supposed to happen*/
+    if (virtqueue_enqueue_buf(DEV_DATA(dev)->vqin, buffer, 1, buffer, length))
+	    return -EIO;
+    virtqueue_notify(DEV_DATA(dev)->vqin);
+    do {
+	    cookie = virtqueue_dequeue_buf(DEV_DATA(dev)->vqin, NULL);
+	    /* TODO: yield or something if not in isr context. might not work, as we're irq locked when called from task context */
+    } while (!cookie);
+    __ASSERT(cookie == buffer, "Got the wrong cookie");
     return 0;
 }
 
+static int virtio_rng_get_entropy(const struct device *dev, uint8_t *buffer, uint16_t length)
+{
+    int key, ret;
+    key = irq_lock();
+    ret = virtio_rng_get_entropy_internal(dev, buffer, length);
+    irq_unlock(key);
+    return ret;
+}
+
+static int virtio_rng_get_entropy_isr(const struct device *dev, uint8_t *buffer, uint16_t length, uint32_t flags)
+{
+    int key, ret;
+    ret = virtio_rng_get_entropy_internal(dev, buffer, length);
+    if (!ret)
+        return length;
+    return ret;
+}
+
 static const struct entropy_driver_api virtio_rng_api = {
-        .get_entropy = virtio_rng_get_entropy
+    .get_entropy = virtio_rng_get_entropy,
+    .get_entropy_isr = virtio_rng_get_entropy_isr,
 };
 
 #define CREATE_VIRTIO_RNG_DEVICE(inst) \
@@ -91,7 +120,7 @@ static const struct entropy_driver_api virtio_rng_api = {
     NULL,\
     &virtio_rng_data_##inst,\
     &virtio_rng_cfg_##inst,\
-    PRE_KERNEL_2/*PRE_KERNEL_1*/,\
+    PRE_KERNEL_1,\
     CONFIG_ENTROPY_INIT_PRIORITY,\
     &virtio_rng_api);
 
